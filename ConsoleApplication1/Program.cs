44fbabb4701afd9ecfa0867156c46d71dfbd137e
V-Kevin <809276891@qq.com>
2015-01-15 22:40:21 +0800

    new

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stu stu = new Stu();
+            stu.Id = 1;
+            Console.WriteLine(stu.Id);
+            Console.Read();
+        }
+    }
+}
